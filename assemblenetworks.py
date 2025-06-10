05ee3de35f6e4cae5e6e63fcf60b0226650bd491
KonstantinosKounakis <94740774+KonstantinosKounakis@users.noreply.github.com>
2025-06-10 15:02:39 +0300

    Finished initial JuPyter addaptation

---
+++ assemblenetworks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+
+checkpoint_SEG = os.path.join('Models','Body','SEG','model.pth')
+checkpoint_SKL = os.path.join('Models','Body','SKL','model.pth')
+
+subprocess.run(["file_split_merge","-m","-i", checkpoint_SEG])
+subprocess.run(["file_split_merge","-m","-i", checkpoint_SKL])
+
